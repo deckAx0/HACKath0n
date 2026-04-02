adbadb826f9015f1acf0f8da0dbd5383eaac0e8f
raine <raine@raine.raine>
2026-04-02 20:10:25 +0000

    initialize flask app, add file upload feature and secure checks in it

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,10 @@
+import os
+from flask import Flask, request
 
+app = Flask(__name__)
+app.config['MAX_FILE_SIZE'] = 50 * 1024 * 1024  # 50 MB
+app.config['UPLOAD_FOLDER'] = 'temp_uploads'
+
+if __name__ == '__main__':    
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
